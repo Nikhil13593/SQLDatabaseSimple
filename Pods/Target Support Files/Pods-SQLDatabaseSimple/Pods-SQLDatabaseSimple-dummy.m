81442f27f7b3465fe9596019922bbbd1705c9a6d
Nikhil Patil <npatil972@gmail.com>
2019-04-02 14:07:54 +0530

    SQLDatabase Demo Try

---
+++ Pods/Target Support Files/Pods-SQLDatabaseSimple/Pods-SQLDatabaseSimple-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SQLDatabaseSimple : NSObject
+@end
+@implementation PodsDummy_Pods_SQLDatabaseSimple
+@end
--- SQLDatabaseSimple/ViewController.swift
+++ SQLDatabaseSimple/ViewController.swift
@@ -7,12 +7,64 @@
 //
 
 import UIKit
+import SQLite
 
 class ViewController: UIViewController {
 
+    var path:String!
+    var db:Connection!
+    
+    @IBOutlet weak var firstName: UITextField!
+    @IBOutlet weak var lastName: UITextField!
+    @IBOutlet weak var age: UITextField!
+    @IBOutlet weak var mobNo: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        path = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+        
+        do{
+            db = try Connection("\(path!)/signupdata.sqlite3")
+            try db.run("Create table if not exists signupdata (id integer primary key autoincrement,FirstName text,LastName text,Age int,MobileNo int)")
+            print(path!)
+        }catch{
+            print(error)
+        }
+        
+    }
+    
+    @IBAction func saveEvent(_ sender: UIButton) {
+        
+        do{
+            try db.run("insert into signupdata (FirstName,LastName,Age,MobileNo) values (?,?,?,?)",firstName.text!,lastName.text!,age.text!,mobNo.text!)
+        }catch{
+            print(error)
+        }
+        
+        firstName.text! = ""
+        lastName.text! = ""
+        age.text! = ""
+        mobNo.text! = ""
+        
+    }
+    
+    @IBAction func retrive(_ sender: UIButton) {
+        
+        do{
+            let statement = try db.run("Select FirstName,LastName,Age,MobileNo from signupdata")
+            
+            for row in statement
+            {
+                for (index,name) in statement.columnNames.enumerated()
+                {
+                    print("\(name):\(row[index]!)")
+                }
+            }
+            
+        }catch{
+            print(error)
+        }
     }
 
 
